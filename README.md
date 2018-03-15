93b17ae27f639b1fd04129eaf3b711a4662ecd69
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-03-16 09:16:29 +1100

    refactor

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 The code from my TW RGB Split Experiment presentation lives here.
 
 This included using one of my Arduino game controllers for slide deck control.
+
+N.B. Not all images optimised - this ran on a local server.
--- js/hardware.js
+++ js/hardware.js
@@ -1,16 +1,5 @@
 // **************** HARDWARE CONTROLLER SOCKET *********************************
 
-// initialize button flags
-// let hardwareL = false;
-// let hardwareR = false;
-// let hardwareU = false;
-// let hardwareD = false;
-//
-// let hardwareL2 = false;
-// let hardwareR2 = false;
-// let hardwareU2 = false;
-// let hardwareD2 = false;
-
 // FYI ARDUINO HARDWARE OUTPUTS:
 // LEFT 48 off, 49 on
 // UP 50 off, 51 on
@@ -31,87 +20,37 @@ socket.on('connect', function () {
 
     socket.on('pressed', function(data) {
       switch(data.pressed) {
-        // PLAYER 1 - LHS USB PORT on test computer
-        case 48:
-          // hardwareL = false;
-          break;
-        case 49:
-          // hardwareL = true;
+        // LHS USB PORT
+        case 49: // L pressed
           prevSlide();
           break;
-        case 50:
-          // hardwareU = false;
-          break;
-        case 51:
-          // hardwareU = true;
+        case 51: // U pressed
           video.play();
           break;
-        case 52:
-          // hardwareR = false;
-          break;
-        case 53:
-          // hardwareR = true;
+        case 53: // R pressed
           nextSlide();
           break;
-        case 54:
-          // hardwareD = false;
-          break;
-        case 55:
-          // hardwareD = true;
+        case 55: // D pressed
           video.volume -= 0.05;
           break;
-        // default:
-          // hardwareL = false;
-          // hardwareR = false;
-          // hardwareU = false;
-          // hardwareD = false;
         }
     });
 
     socket.on('pressed1421', function(data) {
         switch(data.pressed1421) {
-        // PLAYER 2 - RHS USB PORT on test computer
-        case 58:
-          // hardwareL2 = false;
-          break;
-        case 59:
-          // hardwareL2 = true;
+        // RHS USB PORT
+        case 59: // L pressed
           prevSlide();
           break;
-        case 60:
-          // hardwareU2 = false;
-          break;
-        case 61:
-          // hardwareU2 = true;
+        case 61: // U pressed
           video.play();
           break;
-        case 62:
-          // hardwareR2 = false;
-          break;
-        case 63:
-          // hardwareR2 = true;
+        case 63: // R pressed
           nextSlide();
           break;
-        case 64:
-          // hardwareD2 = false;
-          break;
-        case 65:
-          // hardwareD2 = true;
+        case 65: // D pressed
           video.volume -= 0.05;
           break;
-        // default:
-        //   hardwareL2 = false;
-        //   hardwareR2 = false;
-        //   hardwareU2 = false;
-        //   hardwareD2 = false;
         }
       });
 });
-
-
-
-
-
-
-
-//*****************************************************************************
